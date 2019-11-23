5aad07fcbddbb1c5b1f42bb29dedf5eccc73e86c
Erick Arostegui Cunza <scorpius86@gmail.com>
2019-11-23 15:32:08 -0500

    Clase 06

---
+++ 03 - Uso de componentes en tiempo real/Galaxy.RealTime.Ajax/Controllers/HomeController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Galaxy.RealTime.NoSignalR.Models;
+using Microsoft.AspNetCore.Http;
+using System.Threading;
+using System.Net.WebSockets;
+using System.Text;
+
+namespace Galaxy.RealTime.NoSignalR.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public HomeController(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Ajax()
+        {
+            return View();
+        }
+
+        public IActionResult Poll()
+        {
+            return View();
+        }
+
+        public IActionResult LongPoll()
+        {
+            return View();
+        }
+        public IActionResult ServerSentEvents()
+        {
+            return View();
+        }
+
+        public IActionResult WebSocket()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public void ServerNotification()
+        {
+            Response.ContentType = "text/event-stream";
+            Thread.Sleep(3000);
+
+
+            HttpContext.Response.WriteAsync("test");
+            HttpContext.Response.Body.Flush();
+
+            Response.Body.Close();
+        }
+
+        public async void WebSocketNotification()
+        {
+            var context = _httpContextAccessor.HttpContext;
+            if (context.WebSockets.IsWebSocketRequest)
+            {
+                var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                await SendEvents(webSocket, "test");
+                await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure,
+                    "Done", CancellationToken.None);
+            }
+            else
+            {
+                context.Response.StatusCode = 400;
+            }
+        }
+
+        private async Task SendEvents(WebSocket webSocket, string data)
+        {
+
+                Thread.Sleep(2000);
+                var jsonMessage = $"\"{data}\"";
+                await webSocket.SendAsync(buffer: new ArraySegment<byte>(
+                    array: Encoding.ASCII.GetBytes(jsonMessage),
+                    offset: 0,
+                    count: jsonMessage.Length),
+                    messageType: WebSocketMessageType.Text,
+                    endOfMessage: true,
+                    cancellationToken: CancellationToken.None
+                );
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ 03 - Uso de componentes en tiempo real/Galaxy.RealTime.SignalR/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Galaxy.RealTime.SignalR.Models;
+using Galaxy.RealTime.SignalR.Hubs;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Galaxy.RealTime.SignalR.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        private readonly IHubContext<AuthorHub> _authorHub;
+        public HomeController(IHubContext<AuthorHub> authorHub)
+        {
+            _authorHub = authorHub;
+        }
+
+        public async Task<IActionResult> Mensaje()
+        {
+            await _authorHub.Clients.All.SendAsync("MostrarMensaje", new { nemsaje = "text"});
+            return Accepted(1);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
